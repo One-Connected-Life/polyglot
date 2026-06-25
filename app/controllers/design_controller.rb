c3bc405d62265559814a99af84655d15bc2b45c9
Mihai Banulescu <mihai.banulescu@gmail.com>
2026-06-25 10:30:36 +0200

    Design: allow unauthenticated access to /design/app prototype

--- app/controllers/design_controller.rb
+++ app/controllers/design_controller.rb
@@ -2,8 +2,8 @@ class DesignController < ApplicationController
   # Throwaway design preview: render candidate iOS tab bars in the browser so we
   # can pick one WITHOUT cutting a native app build. Unauthenticated + onboarding-
   # exempt so it's openable on the phone with a tap.
-  allow_unauthenticated_access only: :tabs
-  skip_before_action :require_onboarding, only: :tabs
+  allow_unauthenticated_access only: %i[ tabs app ]
+  skip_before_action :require_onboarding, only: %i[ tabs app ]
 
   def tabs
     render layout: false
